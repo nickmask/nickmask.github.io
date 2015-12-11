f2761153d9063e5fa0f2909d94092b8958816d63
nickmask <nrmaskell@gmail.com>
2015-12-11 14:45:37 +1300

    fix insult text

--- js/the-insulter.js
+++ js/the-insulter.js
@@ -7,7 +7,7 @@ function insult() {
 
 function insultDecider() {
 	if (decider < 0.1) {
-		document.getElementById("insultOutput").innerHTML = "I'm not saying I hate you, " + name +" but I would unplug your life support to charge my phone."
+		document.getElementById("insultOutput").innerHTML = "I'm not saying I hate you " + name +", but I would unplug your life support to charge my phone."
 	} else if (decider < 0.2) {
 		document.getElementById("insultOutput").innerHTML = "Hey " + name + ", your birth certificate is an apology letter from the condom factory."
 	} else if (decider < 0.3) {
@@ -15,9 +15,9 @@ function insultDecider() {
 	} else if (decider < 0.4) {
 		document.getElementById("insultOutput").innerHTML = "Hey " + name + ", is your ass jealous of the amount of shit that just came out of your mouth?";
 	} else if (decider < 0.5) {
-		document.getElementById("insultOutput").innerHTML = name + " is so ugly, when your mom dropped you off at school she got a fine for littering.";	
+		document.getElementById("insultOutput").innerHTML = name + " you're so ugly, when your mom dropped you off at school she got a fine for littering.";	
 	} else if (decider < 0.6) {
-		document.getElementById("insultOutput").innerHTML = "The only way " + name + " will ever get laid is if you crawl up a chicken's ass and wait.";
+		document.getElementById("insultOutput").innerHTML = "The only way " + name + " will ever get laid is if she crawl up a chicken's ass and waited.";
 	} else if (decider < 0.7) {
 		document.getElementById("insultOutput").innerHTML = "Hey " + name + ", it looks like your face caught on fire and someone tried to put it out with a fork.";
 	} else if (decider < 0.8) {
