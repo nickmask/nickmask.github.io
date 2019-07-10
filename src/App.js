dfbf8dba2e8e0d7a368b47adba36c063f0ed45f7
Nick Maskell <nrmaskell@gmail.com>
2019-07-10 15:33:28 +0100

    Make base css-grid layout

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+        <header>Header</header>
+        <main>Main content</main>
+        <footer>Footer</footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
