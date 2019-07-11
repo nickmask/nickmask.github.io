02cc05e780f86e2cf2205249bdb373587a29cb82
Nick Maskell <nrmaskell@gmail.com>
2019-07-11 13:54:05 +0100

    Updates

---
+++ precache-manifest.5ce566f7eb56f014f4c7701a8aad9f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6687c79717ee1b2bb9c7a05b6306c08d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b146ec1b859d067346ef",
+    "url": "/static/css/main.7a9aa41f.chunk.css"
+  },
+  {
+    "revision": "ef17083c547e3e349cd2",
+    "url": "/static/js/2.7df9eb00.chunk.js"
+  },
+  {
+    "revision": "b146ec1b859d067346ef",
+    "url": "/static/js/main.23a0169c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca8632f4d0aacae4633cbfc2f1a673f1",
+    "url": "/static/media/Anurati-Regular.ca8632f4.otf"
+  }
+]);
